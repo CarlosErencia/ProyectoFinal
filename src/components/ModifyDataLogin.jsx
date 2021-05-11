318a91b61510bbd92259db2f2aab186a102877d6
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2021-05-11 08:55:28 +0200

    prueba recuperar datos login

---
+++ src/components/ModifyDataLogin.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+const FormContacto = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { Nombre, email, Mensaje } = e.target.elements;
+    let details = {
+      Nombre: Nombre.value,
+      email: email.value,
+      Mensaje: Mensaje.value,
+    };
+    let response = await fetch("http://localhost:3000/contacto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+  return (
+      <div class="login">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="Nombre">Nombre:</label>
+                    <input type="text" id="Nombre" required />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" required />
+                </div>
+                <button class="boton" type="submit">{status}</button>
+            </form>
+      </div>
+    
+  );
+};
+
+export default FormContacto;
---
+++ src/pages/ModifyDataLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from '../components/ModifyDataLogin'
+
+const ModifyDataLogin = () => {
+    return (
+        <ModifyDataLogin/>
+    )
+}
+export default ModifyDataLogin;
