33cfb0bcce60d89c805c305dea5817b3f10cbc32
CarlosErencia <a19carerecas@inspedralbes.cat>
2021-05-12 10:34:15 +0200

    Style Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
-
+import './style/Login.css'
 
 const Login = () => {
 
@@ -9,9 +9,21 @@ const Login = () => {
     console.log(response);
   }
 
-    const responseGoogle = (response) => {
-        console.log(response);
-      }
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  const signUpButton = document.getElementById('signUp');
+  const signInButton = document.getElementById('signIn');
+  const container = document.getElementById('containerLogin');
+
+  signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+  });
+
+  signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+  });
 
     return (
        <div className="App login">
@@ -33,20 +45,42 @@ const Login = () => {
             icon="fa-facebook" 
           />
          </div>
-         <div class="form-popup" id="myForm">
-          <form action="/action_page.php" class="form-container">
-          <h1>Registro Manual</h1>
-
-          <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required></input>
-
-          <label for="psw"><b>Contraseña</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required></input>
-
-          <button type="submit" class="btn">Aceptar</button>
-          <button type="submit" class="btn cancel" onclick="closeForm()">Cancelar</button>
-        </form>
-        </div>
+          <div class="containerLogin" id="container">
+            <div class="form-container sign-up-container">
+              <form action="#">
+                <h1>Create Account</h1>
+                <span>or use your email for registration</span>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign Up</button>
+              </form>
+            </div>
+            <div class="form-container sign-in-container">
+              <form action="#">
+                <h1>Sign in</h1>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+              </form>
+            </div>
+            <div class="overlay-container">
+              <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                  <h1>Welcome Back!</h1>
+                  <p>To keep connected with us please login with your personal info</p>
+                  <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                  <h1>Hello, Friend!</h1>
+                  <p>Enter your personal details and start journey with us</p>
+                  <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+              </div>
+            </div>
+          </div>    
         </div>
     )
   }
