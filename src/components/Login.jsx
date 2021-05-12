7f77c3b7b2f56df415d72a6fd451ca7054cc1de1
CarlosErencia <a19carerecas@inspedralbes.cat>
2021-05-12 11:10:17 +0200

    login style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,37 +13,40 @@ const Login = () => {
     console.log(response);
   }
 
-  const signUpButton = document.getElementById('signUp');
-  const signInButton = document.getElementById('signIn');
-  const container = document.getElementById('containerLogin');
+  // const signUpButton = document.getElementById('signUp');
+  // const signInButton = document.getElementById('signIn');
+  // const container = document.getElementById('containerLogin');
 
-  signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-  });
+  // signUpButton.addEventListener('click', () => {
+  //   container.classList.add("right-panel-active");
+  // });
 
-  signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-  });
+  // signInButton.addEventListener('click', () => {
+  //   container.classList.remove("right-panel-active");
+  // });
 
     return (
-       <div className="App login">
-         <div class="Google">
-          <GoogleLogin 
-            clientId="831405529059-rpn2t8cetre42e7qjst57alphauch37d.apps.googleusercontent.com"
-            buttonText="Iniciar Sesion"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
+       <div className="Applogin">
+         <div class="BotonesLogin">
+          <div class="Google">
+            <GoogleLogin 
+              clientId="831405529059-rpn2t8cetre42e7qjst57alphauch37d.apps.googleusercontent.com"
+              buttonText="Iniciar Sesion"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              />
+          </div>
+          <div class="facebook">
+            <FacebookLogin
+              appId="2043028465844725"
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook" 
             />
          </div>
-         <div class="facebook">
-          <FacebookLogin
-            appId="2043028465844725"
-            fields="name,email,picture"
-            scope="public_profile,user_friends"
-            callback={responseFacebook}
-            icon="fa-facebook" 
-          />
+        
          </div>
           <div class="containerLogin" id="container">
             <div class="form-container sign-up-container">
