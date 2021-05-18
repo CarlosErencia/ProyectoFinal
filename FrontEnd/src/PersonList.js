dc72db6b0f564d5351157406c7335604d5fbf403
a17gerchifer <[a17gerchifer@inspedralbes.cat]>
2021-05-18 12:49:06 +0200

    person list con json actualizados

--- FrontEnd/src/PersonList.js
+++ FrontEnd/src/PersonList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+//import defaultImg from 'src/images/room-1.jpeg';
+
 
 import axios from 'axios';
 
@@ -23,18 +26,25 @@ export default class PersonList extends React.Component {
     return (
     
       <ul>
-        { this.state.persons.map(person => <li>{person.name}</li>)}
-        { this.state.persons.map(person => <li>{person.slug}</li>)}
-        { this.state.persons.map(person => <li>{person.type}</li>)}
-        { this.state.persons.map(person => <li>{person.price}</li>)}
-        { this.state.persons.map(person => <li>{person.size}</li>)}
-        { this.state.persons.map(person => <li>{person.capacity}</li>)}
-        { this.state.persons.map(person => <li>{person.pets}</li>)}
-        { this.state.persons.map(person => <li>{person.breakfast}</li>)}
-        { this.state.persons.map(person => <li>{person.featured}</li>)}
-        { this.state.persons.map(person => <li>{person.description}</li>)}
-        { this.state.persons.map(person => <li>{person.extras}</li>)}
+        { this.state.persons.map(person => 
+                  <div>
+                  <div className="col-md-4 col-12 mx-auto p-2">
+                  <div className="card shadow-lg border-0 room">
+                  {/* <img src={person.images[0] || defaultImg} alt="single room" className="img-fluid"/> */}
+                      <div className="price-top">
+                          <h6>{person.price}€</h6>
+                          <p>la noche</p>
+                      </div>
+                      <Link to={`/rooms/${person.slug}`} className="btn-warning room-link text-center" >Features</Link>
+                    <p className="room-info">{person.name}</p>
+                  </div>
+              </div>
+              </div>
+
+        )}
+       
       </ul>
     )
   }
 }
+
