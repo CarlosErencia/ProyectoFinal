97eb58a10197ca557932dcbc9f0460b891332237
a14joerodnad <a14joerodnad@inspedralbes.cat>
2021-05-20 13:46:52 +0200

    PRACTICAS GERARD I JOEL (installations i services)

---
+++ FrontEnd/src/components/Servicios.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+//import defaultImg from 'src/images/room-1.jpeg';
+import axios from 'axios';
+import Title from './Title'
+
+export default class Servicios extends Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://192.168.210.150:4000/api/services`)
+      .then(res => {
+        const persons = res.data;
+        console.log(res.data);
+        this.setState({ persons });
+
+      }).catch(function (){
+          console.error("tdfghkkjg")
+      })
+  }
+
+  render() {
+    return (
+        <div className="container-fluid services">
+         <Title title="Servicios" />
+            <div className="row">
+               {this.state.persons.map((person, index) => {
+                  return(
+                    <div className="col-md-4 col-lg-3 col-12 mx-auto my-3" key={index}>
+                        <div className="card shadow-lg border-0 p-4">
+                            <article className="service">
+                            <span>{person.imagen}</span>
+                            <h6>{person.name}</h6>
+                            <p>{person.description}</p>
+                            </article>              
+                       </div>
+                    </div>
+                  )
+               })}
+            </div>
+        </div>
+    )
+}
+}
+
