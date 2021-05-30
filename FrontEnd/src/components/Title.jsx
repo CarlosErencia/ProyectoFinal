76c07ecc9e93e7d0cdd788846fc5ef419f829dd2
a14joerodnad <73338094+a14joerodnad@users.noreply.github.com>
2021-05-30 18:22:23 +0200

    comentari title.jsx

--- FrontEnd/src/components/Title.jsx
+++ FrontEnd/src/components/Title.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// Aquest simplement es un fitxer on el cridan varios arxius per poder ficar un titol amb un estil ja assignat
+
 export default function Title({title}) {
     return (
         <div className="section-title">
