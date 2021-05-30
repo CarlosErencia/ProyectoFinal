686f46f9b476946c37108080fe893d82cc22de2e
a14joerodnad <73338094+a14joerodnad@users.noreply.github.com>
2021-05-30 18:24:53 +0200

    comentari login-button.js

--- FrontEnd/src/components/login-button.js
+++ FrontEnd/src/components/login-button.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
+// Aquest es el boto per poder loguejarte
+
 const LoginButton = () => {
     const {loginWithRedirect} = useAuth0();
     return(
