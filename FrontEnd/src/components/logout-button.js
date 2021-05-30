2ed5630bed64acfbbf85ddaf886ae8b35025bd62
a14joerodnad <73338094+a14joerodnad@users.noreply.github.com>
2021-05-30 18:25:52 +0200

    comentari logout-button.js

--- FrontEnd/src/components/logout-button.js
+++ FrontEnd/src/components/logout-button.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
+// Aquest el boto per poder desloguejarte un cop has fet el login
+
 const LogoutButton = () => {
     const {logout} = useAuth0();
     return(
