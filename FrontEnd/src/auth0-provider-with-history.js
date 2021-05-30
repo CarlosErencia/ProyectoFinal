6606af2492df981888d047629e5c4db99beb0804
JordiCervera <73636664+JordiCervera@users.noreply.github.com>
2021-05-30 17:58:00 +0200

    Comentari sobre login auth0

--- FrontEnd/src/auth0-provider-with-history.js
+++ FrontEnd/src/auth0-provider-with-history.js
@@ -1,3 +1,4 @@
+//Aquest fitxer té la funcio de redirigir, una vegada autentifica l'usuari logejat
 import React from "react";
 import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
